3dbb6d10b09d2c81d6a1021b973ac43ad5fd94bb
lirena <sakshamkushwaha12776@gmail.com>
2024-02-27 19:57:54 +0530

    added fonts and updated homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,76 @@ import Typewriter from 'typewriter-effect';
 import Link from "next/link";
 import Image from 'next/image'
 
+interface Project {
+  href: string;
+  imageSrc: string;
+  title: string;
+  description: string;
+}
+
+const projects: Project[] = [
+  {
+    href: "https://aniray.lirena.xyz",
+    imageSrc: "/aniray_banner.jpg",
+    title: "Aniray",
+    description: "Aniray is an anime streaming site which I used to get into Next.js and Tailwind CSS. It also focuses on the scraping of anime from various sources and displaying them in a clean and easy to use interface."
+  },
+  {
+    href: "https://top.gg/bot/855532494035484743",
+    imageSrc: "/clover.jpg",
+    title: "Clover Bot",
+    description: "Clover is a Discord bot which I made to learn more about Python and Discord.py and Database. It had a lot of features, including a leveling system, a currency system, and a lot more. It also had several api integrations."
+  },
+  {
+    href: "https://story.lirena.xyz",
+    imageSrc: "/story.jpg",
+    title: "Story",
+    description: "This is a website which I made to explore more about libraries like contentlayer and tailwindcss This also have a collection of several of the stories which I have written as the part of my hobby."
+  },
+  {
+    href: "https://aishiteru.lirena.xyz",
+    imageSrc: "/aishiteru.jpg",
+    title: "Aishiteru",
+    description: "A rewrite and a better next.js version of one of the first project with same name. This was built with T3 stack and uses TS. It's like Tiktok but for waifu lovers. It has endless scroll of Waifu Images."
+  },
+  {
+    href: "",
+    imageSrc: "/tepi.jpg",
+    title: "Tepi",
+    description: "A multipurpose private api used in several sites of mine for example Aniray and Story. It is built using flask and Python Mostly it serves as an image generation api which have several endpoints for different types of images."
+  },
+  {
+    href: "",
+    imageSrc: "/siesta.jpg",
+    title: "Siesta",
+    description: "A priavte music discord bot only for some servers which is not discontinued. It was built on the same framework as Clover, but it was more focused on music and had a lot of features related to music."
+  },
+  {
+    href: "https://github.com/LiReNa00/Manga-Miner",
+    imageSrc: "/manga_miner.jpg",
+    title: "Manga Miner",
+    description: "This was one of the first project that I built for scraping manga from various sources. It was built using Python and BeautifulSoup. This also had a feature to download the manga in the form of a pdf."
+  },
+  {
+    href: "",
+    imageSrc: "/dash.jpg",
+    title: "Dash",
+    description: "An in development homepage for my browser which is built using Next.js and Tailwind CSS. With features like quicklinks, weather and in development features like todo list and more."
+  },
+];
+
+const ProjectCard: React.FC<Project> = ({ href, imageSrc, title, description }) => (
+  <Link href={href} target="_blank">
+    <div className="rounded-xl overflow-hidden group relative z-20 text-white h-full">
+      <Image className="w-full object-cover h-full absolute -z-10 transform group-hover:scale-110 duration-300" src={imageSrc} alt="Project Banner" width={1000} height={1000}/>
+      <div className="px-6 py-4 card h-40 grid place-items-left">
+        <div className="font-semibold text-xl mb-2 text-left ">{title}</div>
+        <p className="text-gray-200 text-base w-full">{description}</p>
+      </div>
+    </div>
+  </Link>
+);
+
 
 const Home: NextPage = ()=>{
   
@@ -54,113 +124,9 @@ const Home: NextPage = ()=>{
      
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 justify-center items-center">
 
-        <Link href="https://aniray.lirena.xyz" target="_blank" >
-        <div className=" rounded overflow-hidden transform hover:scale-[0.98] duration-300 card h-full text-white w-full">
-        <Image className="w-full object-cover" src="/aniray_banner.jpg" alt="Project Banner" width={1000} height={1000}/>
-        <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">Aniray</div>
-        <p className="text-gray-400 text-base h-max ">
-        Aniray is an anime streaming site which I used to get into Next.js and Tailwind CSS. It also focuses on the scraping
-        of anime from various sources and displaying them in a clean and easy to use interface.
-        </p>
-        </div>
-        </div>
-        </Link>
-
-
-
-        <Link href="https://top.gg/bot/855532494035484743" target="_blank">
-        <div className=" rounded overflow-hidden transform hover:scale-[0.98] duration-300  card text-white">
-        <Image className="w-full object-cover" src="/clover.jpg" alt="Project Banner" width={1000} height={1000}/>
-        <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">Clover Bot</div>
-        <p className="text-gray-400 text-base w-full">
-        Clover is a Discord bot which I made to learn more about Python and Discord.py and Database. It had a lot of
-        features, including a leveling system, a currency system, and a lot more. It also had several api integrations.
-        </p>
-        </div>
-        </div>
-        </Link>
-
-        <Link href="https://story.lirena.xyz" target="_blank">
-        <div className=" rounded overflow-hidden transform hover:scale-[0.98] duration-300  card text-white h-full">
-        <Image className="w-full object-cover" src="/story.jpg" alt="Project Banner" width={1000} height={1000}/>
-        <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">Story</div>
-        <p className="text-gray-400 text-base w-full">
-        This is a website which I made to explore more about libraries like contentlayer and tailwindcss This also have a collection
-        of several of the stories which I have written as the part of my hobby. 
-        </p>
-        </div>
-        </div>
-        </Link>
-
-        <Link href="https://aishiteru.lirena.xyz" target="_blank">
-        <div className=" rounded overflow-hidden transform hover:scale-[0.98] duration-300  card text-white">
-        <Image className="w-full object-cover" src="/aishiteru.jpg" alt="Project Banner" width={1000} height={1000}/>
-        <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">Aishiteru</div>
-        <p className="text-gray-400 text-base w-full ">
-        A rewrite and a better next.js version of one of the first project with same name. This was built with T3 stack and
-        uses TS. It&apos;s like Tiktok but for waifu lovers. It has endless scroll of Waifu Images.
-        </p>
-        </div>
-        </div>
-        </Link>
-
-        <Link href="" >
-        <div className=" rounded overflow-hidden transform hover:scale-[0.98] duration-300  card text-white">
-        <Image className="w-full object-cover" src="/tepi.jpg" alt="Project Banner" width={1000} height={1000}/>
-        <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">Tepi</div>
-        <p className="text-gray-400 text-base w-full ">
-         A multipurpose private api used in several sites of mine for example Aniray and Story. It is built using flask and Python
-        Mostly it serves as an image generation api which have several endpoints for different types of images.
-        </p>
-        </div>
-        </div>
-        </Link>
-
-        <Link href="">
-        <div className=" rounded overflow-hidden transform hover:scale-[0.98] duration-300  card text-white">
-        <Image className="w-full object-cover" src="/siesta.jpg" alt="Project Banner" width={1000} height={1000}/>
-        <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">Siesta</div>
-        <p className="text-gray-400 text-base w-full ">
-        A priavte music discord bot only for some servers which is not discontinued. It was built on the same framework as Clover,
-        but it was more focused on music and had a lot of features related to music.
-        </p>
-        </div>
-        </div>
-        </Link>
-
-        <Link href="https://github.com/LiReNa00/Manga-Miner" target="_blank">
-        <div className=" rounded overflow-hidden transform hover:scale-[0.98] duration-300  card text-white">
-        <Image className="w-full object-cover" src="/manga_miner.jpg" alt="Project Banner" width={1000} height={1000}/>
-        <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">Manga Miner</div>
-        <p className="text-gray-400 text-base w-full ">
-        This was one of the first project that I built for scraping manga from various sources. It was built using Python and BeautifulSoup.
-        This also had a feature to download the manga in the form of a pdf.
-        </p>
-        </div>
-        </div>
-        </Link>
-
-        <Link href="" >
-        <div className=" rounded overflow-hidden transform hover:scale-[0.98] duration-300  card text-white">
-        <Image className="w-full object-cover" src="/dash.jpg" alt="Project Banner" width={1000} height={1000}/>
-        <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">Dash</div>
-        <p className="text-gray-400 text-base w-full ">
-        An in development homepage for my browser which is built using Next.js and Tailwind CSS. With features like
-        quicklinks, weather and in development features like todo list and more.
-        </p>
-        </div>
-        </div>
-        </Link>
-
-
+        {projects.map((project,index) => (
+          <ProjectCard {...project} key={index} />
+        ))}
         </div>
         <p className="text-3xl font-semibold  text-white">
         Tech Stack
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'ubuntu': ['ubuntu', 'sans-serif'],
+         }
+    },
   },
   plugins: [],
 } satisfies Config;
