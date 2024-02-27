9ec3d1bd205a9029980bd25621b044a3f096aa56
lirena <sakshamkushwaha12776@gmail.com>
2024-02-27 18:07:16 +0530

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ const Home: NextPage = ()=>{
 
         <p className="text-3xl font-semibold tracking-tight inline text-white">
   Hi, I&apos;m{' '}
-  <span className="inline text-[hsl(280,100%,70%)]">
+  <span className="inline text-[#cc66ff] ">
     <Typewriter
       options={{
         strings: ['Lirena', 'Saksham Kushwaha'],
@@ -31,6 +31,7 @@ const Home: NextPage = ()=>{
       }}
     />
   </span>
+
 </p>
 
 
@@ -54,7 +55,7 @@ const Home: NextPage = ()=>{
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 justify-center items-center">
 
         <Link href="https://aniray.lirena.xyz" target="_blank" >
-        <div className=" rounded overflow-hidden transform hover:scale-[0.98] duration-300 card text-white w-full">
+        <div className=" rounded overflow-hidden transform hover:scale-[0.98] duration-300 card h-full text-white w-full">
         <Image className="w-full object-cover" src="/aniray_banner.jpg" alt="Project Banner" width={1000} height={1000}/>
         <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">Aniray</div>
@@ -165,7 +166,7 @@ const Home: NextPage = ()=>{
         Tech Stack
         </p>
 
-        <div className="grid grid-cols-4 lg:grid-cols-9 gap-2 mb-10 ">
+        <div className="grid grid-cols-4 gap-2 mb-10 lg:flex lg:flex-wrap">
         
         <div className="p-2 card flex   rounded-xl  w-fit">
         <svg xmlns="http://www.w3.org/2000/svg" width="5em" height="5em" viewBox="0 0 32 32"><defs><linearGradient id="vscodeIconsFileTypePython0" x1="-133.268" x2="-133.198" y1="-202.91" y2="-202.84" gradientTransform="matrix(189.38 0 0 189.81 25243.061 38519.17)" gradientUnits="userSpaceOnUse"><stop offset="0" stopColor="#387eb8"></stop><stop offset="1" stopColor="#366994"></stop></linearGradient><linearGradient id="vscodeIconsFileTypePython1" x1="-133.575" x2="-133.495" y1="-203.203" y2="-203.133" gradientTransform="matrix(189.38 0 0 189.81 25309.061 38583.42)" gradientUnits="userSpaceOnUse"><stop offset="0" stopColor="#ffe052"></stop><stop offset="1" stopColor="#ffc331"></stop></linearGradient></defs><path fill="url(#vscodeIconsFileTypePython0)" d="M15.885 2.1c-7.1 0-6.651 3.07-6.651 3.07v3.19h6.752v1H6.545S2 8.8 2 16.005s4.013 6.912 4.013 6.912H8.33v-3.361s-.13-4.013 3.9-4.013h6.762s3.772.06 3.772-3.652V5.8s.572-3.712-6.842-3.712Zm-3.732 2.137a1.214 1.214 0 1 1-1.183 1.244v-.02a1.214 1.214 0 0 1 1.214-1.214Z"></path><path fill="url(#vscodeIconsFileTypePython1)" d="M16.085 29.91c7.1 0 6.651-3.08 6.651-3.08v-3.18h-6.751v-1h9.47S30 23.158 30 15.995s-4.013-6.912-4.013-6.912H23.64V12.4s.13 4.013-3.9 4.013h-6.765S9.2 16.356 9.2 20.068V26.2s-.572 3.712 6.842 3.712h.04Zm3.732-2.147A1.214 1.214 0 1 1 21 26.519v.03a1.214 1.214 0 0 1-1.214 1.214h.03Z"></path></svg>
