70dada772cc87a6a39934c2ecb24ad7c9baed31a
lirena <sakshamkushwaha12776@gmail.com>
2024-03-19 09:59:54 +0530

    added new projects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,6 +57,18 @@ const projects: Project[] = [
     description: "This was one of the first project that I built for scraping manga from various sources. It was built using Python and BeautifulSoup. This also had a feature to download the manga in the form of a pdf."
   },
   {
+    href: "https://newsense.lirena.xyz",
+    imageSrc: "/newsense.jpg",
+    title: "Newsense",
+    description:"Newsense is a news aggregator website built using Next.js and Tailwind CSS which focuses on saving your time by using power of AI to summarize the articles in points."
+  },
+  {
+    href:"#",
+    imageSrc: "/portfolio.jpg",
+    title: "Portfolio",
+    description: "This site is made using Next.js and Tailwind CSS which serves as portfolio. It showcases all the major projects I have worked on along with techstack and about me."
+  },
+  {
     href: "#",
     imageSrc: "/dash.jpg",
     title: "Dash",
@@ -171,7 +183,9 @@ const Home: NextPage = ()=>{
 
         <div className="relative group p-2 flex h-fit items-center rounded-lg w-fit  bg-gradient-to-t  from-orange-500/40 from-5%  to-gray-800 to-100%">
         <svg xmlns="http://www.w3.org/2000/svg" width="4em" height="4em" viewBox="0 0 32 32"><path fill="#dd4c35" d="M29.472 14.753L17.247 2.528a1.8 1.8 0 0 0-2.55 0l-2.539 2.539l3.22 3.22a2.141 2.141 0 0 1 2.712 2.73l3.1 3.1a2.143 2.143 0 1 1-1.285 1.21l-2.895-2.895v7.617a2.141 2.141 0 1 1-1.764-.062V12.3a2.146 2.146 0 0 1-1.165-2.814l-3.17-3.172L2.528 14.7a1.8 1.8 0 0 0 0 2.551l12.225 12.221a1.8 1.8 0 0 0 2.55 0L29.472 17.3a1.8 1.8 0 0 0 0-2.551"></path><path fill="#fff" d="m12.158 5.067l3.22 3.22a2.141 2.141 0 0 1 2.712 2.73l3.1 3.1a2.143 2.143 0 1 1-1.285 1.21l-2.895-2.895v7.617a2.141 2.141 0 1 1-1.764-.062V12.3a2.146 2.146 0 0 1-1.165-2.814l-3.17-3.172"></path></svg>
-
+        <div className="absolute -top-2  left-1/2 transform -translate-x-1/2 -translate-y-full bg-black text-center text-white text-xs rounded px-2 py-1.5 w-full  opacity-0 group-hover:opacity-100 pointer-events-none transition-opacity duration-300">
+        Git
+    </div>
         </div>
 
         <div className="p-2 flex h-fit items-center rounded-lg w-fit  bg-gradient-to-t  from-teal-600/40 from-5%  to-gray-800 to-100%">
