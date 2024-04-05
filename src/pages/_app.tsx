e69ad41a0ecc1d5f0fd9be29b68ed14f8ebddde4
lirena <sakshamkushwaha12776@gmail.com>
2024-04-05 22:02:13 +0530

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,9 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import '~/styles/globals.css';
+import { type AppType } from "next/dist/shared/lib/utils";
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <Head>
-        <script
-          defer
-          src="https://analytics.lirena.xyz/script.js"
-          data-website-id="7d9f0467-9f61-4fb1-ac4f-6d712106dec5"
-        ></script>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+import "~/styles/globals.css";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;
