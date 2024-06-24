9cf639ea599d309733231121d694c4a24529643c
lirena <sakshamkushwaha12776@gmail.com>
2024-06-24 22:52:56 +0530

    chore: added figma

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -487,6 +487,36 @@ const Home: NextPage = () => {
               </svg>
             </div>
 
+            <div className="flex h-fit w-fit items-center rounded-lg bg-gradient-to-t  from-green-600/50  from-5% to-gray-800  to-100% p-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="4em"
+                height="4em"
+                viewBox="0 0 256 384"
+              >
+                <path
+                  fill="#0ACF83"
+                  d="M64 384c35.328 0 64-28.672 64-64v-64H64c-35.328 0-64 28.672-64 64s28.672 64 64 64"
+                ></path>
+                <path
+                  fill="#A259FF"
+                  d="M0 192c0-35.328 28.672-64 64-64h64v128H64c-35.328 0-64-28.672-64-64"
+                ></path>
+                <path
+                  fill="#F24E1E"
+                  d="M0 64C0 28.672 28.672 0 64 0h64v128H64C28.672 128 0 99.328 0 64"
+                ></path>
+                <path
+                  fill="#FF7262"
+                  d="M128 0h64c35.328 0 64 28.672 64 64s-28.672 64-64 64h-64z"
+                ></path>
+                <path
+                  fill="#1ABCFE"
+                  d="M256 192c0 35.328-28.672 64-64 64s-64-28.672-64-64s28.672-64 64-64s64 28.672 64 64"
+                ></path>
+              </svg>
+            </div>
+
             <div className="flex h-fit w-fit items-center rounded-lg bg-gradient-to-t  from-orange-700/40  from-5% to-gray-800  to-100% p-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
