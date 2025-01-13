51d819bbdb1c727894ab0d0c89b7bdc05894d1a8
lirena <sakshamkushwaha12776@gmail.com>
2025-01-14 00:41:10 +0530

    feat: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,13 @@ import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Saksham Kushwaha | lirena00 | Portfolio",
+  description:
+    "Saksham Kushwaha (lirena00), a 19-year-old CS undergrad, is a full-stack developer, programmer, and artist passionate about AI, web development, and creative storytelling. A weeb at heart, I enjoy reading manga, watching anime, and working on my own post-apocalyptic story, soon to be published. Explore projects like Odash, Animood, open-source contributions, and unique anime-inspired tools.",
+  icons: [{ rel: "icon", url: "/favicon.jpg" }],
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
